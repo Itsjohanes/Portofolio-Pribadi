5caea4d64e2cab3e65ef67f9b16fa0ec84c6d8a7
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-02-09 11:50:40 +0700

    selesai

---
+++ src/constants/constants.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "Kebunku Web App",
+    description:
+      "Kebunku is a web application to help UMKM, especially selling plants",
+    image: "/images/kebunku.jpg",
+    tags: ["PHP", "MySQL", "Bootstrap", "JQuery"],
+    source: "https://github.com/Itsjohanes/Kebunku",
+    visit: "https://kebunku-necode.000webhostapp.com/",
+    id: 0,
+  },
+
+  {
+    title: "GoKlinik (Admin)",
+    description:
+      "goklinik is an application that makes it easy to queue and buy drugs patients through the internet",
+    image: "/images/eb.jpg",
+    tags: ["Codeigniter", "MySQL", "Bootstrap", "JQuery"],
+    source: "https://github.com/Itsjohanes/goklinikweb",
+    visit: "https://goklinik.000webhostapp.com/",
+    id: 2,
+  },
+];
+
+export const TimeLineData = [
+  {
+    year: 2016,
+    text: "Learn Visual Basic Programming Language",
+  },
+  { year: 2017, text: "Learn C# Programming Language" },
+  { year: 2020, text: "Learned C Algorithm & Data Structure" },
+  { year: 2021, text: "Learn PHP Programming Language" },
+  { year: 2022, text: "Learn Java & Android Programmming" },
+];
